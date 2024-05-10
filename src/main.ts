ce0d2a207eb8f6fae66b9415d6441209594ae8b2
Daniel Whiffing <danielwhiffing@gmail.com>
2024-05-10 19:52:58 -0400

    Fixes scaling

--- src/main.ts
+++ src/main.ts
@@ -7,12 +7,13 @@ import { AUTO, Game, Scale, Types } from 'phaser'
 //  https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
 const config: Types.Core.GameConfig = {
   type: AUTO,
-  width: 1024,
-  height: 768,
+  width: 320,
+  height: 200,
+  pixelArt: true,
   parent: 'game-container',
   backgroundColor: '#028af8',
   scale: {
-    mode: Scale.FIT,
+    zoom: Phaser.Scale.MAX_ZOOM,
     autoCenter: Scale.CENTER_BOTH,
   },
   scene: [Boot, Menu, GameScene],
